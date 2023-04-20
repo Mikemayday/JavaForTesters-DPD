36cb0e89dfd45237b74b08bc9fa50bdf2c999986
Michał Madej <mayday.green@gmail.com>
2023-04-20 17:56:29 +0200

    Zrobiłem pierwsze cztery ćwiczenia

--- src/main/java/a_introduction/App.java
+++ src/main/java/a_introduction/App.java
@@ -2,7 +2,6 @@ package a_introduction;
 
 public class App {
     public static void main(String[] args) {
-
     }
 
     public static void drive(Vehicle v) {
--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -2,5 +2,15 @@ package a_introduction;
 
 public class Task1 {
     public static void main(String[] args) {
+        int a = 10;
+        char b = 'd';
+        double c = 40d;
+        long d = 235432453;
+        boolean e = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
